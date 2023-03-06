0db8c4145b1a3d11b6bcf6107d8a33efaa9dce2d
Miguel <aladm1@student.op.ac.nz>
2023-03-06 15:42:19 +1300

    add register login components

---
+++ web-domain-analytics/src/components/Login.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>WordCloud</div>
+  )
+}
+
+export default Login
---
+++ web-domain-analytics/src/components/Register.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div>WordCloud</div>
+  )
+}
+
+export default Register
