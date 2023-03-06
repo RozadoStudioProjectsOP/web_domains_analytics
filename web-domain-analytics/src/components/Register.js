6e790cf4d0b09320e20b5907afb54e422332a0a7
Miguel <aladm1@student.op.ac.nz>
2023-03-06 16:18:22 +1300

    add to register form

--- web-domain-analytics/src/components/Register.js
+++ web-domain-analytics/src/components/Register.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import { useRef, useState, useEffect } from 'react';
+import { Alert, Button, Form, FormGroup, Input } from 'reactstrap'
 
 const Register = () => {
+  const dataRef = useRef()
+
+  const handleSubmit = () => {
+
+  } 
+
   return (
-    <div>WordCloud</div>
+    <Form onSubmit={handleSubmit}>
+        <FormGroup>
+            <Input
+                type='email'
+                placeholder='E-mail' 
+                ref={dataRef}
+                required
+            />
+            <Input
+                type='password'
+                placeholder='Password' 
+                ref={dataRef}
+                required
+            />
+        </FormGroup>
+    </Form>
   )
 }
 
