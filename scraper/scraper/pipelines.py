571a0d2ce70b5a797c1df37b81cfeef900b58682
LennoxHB <bladlh1@student.op.ac.nz>
2023-03-18 21:57:42 +1300

    feat: get the frequency of words in a crawl.

--- scraper/scraper/pipelines.py
+++ scraper/scraper/pipelines.py
@@ -1,13 +1,25 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+#Prcoess the data from the spider into a dictionary that counts the freqency of words.
 
-
-# useful for handling different item types with a single interface
+import json
+import re
 from itemadapter import ItemAdapter
 
-
 class ScraperPipeline:
+    wordCount = {}
+
+    #Once the spider is finished, write contents of wordCount to JSON file.
+    def close_spider(self, spider):
+        self.file = open('data.json', 'w')
+        line = json.dumps(self.wordCount) + "\n"
+        self.file.write(line)
+        self.file.close()
+
+    #Updates wordCount with data extracted from spider.
     def process_item(self, item, spider):
-        return item
+        data = ''.join(item['text']).lower().split()
+        for word in data:
+            subWord = re.sub('[^a-z]', '', word)
+            if subWord in self.wordCount:
+                self.wordCount[subWord] += 1
+            else:
+                self.wordCount[subWord] = 1
