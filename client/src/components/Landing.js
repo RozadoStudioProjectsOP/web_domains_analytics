f02b8471e3e3eb0b3b09c6edbd042882088945e8
Miguel <aladm1@student.op.ac.nz>
2023-04-02 22:15:49 +1200

    (feat) output word number

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,6 +1,6 @@
 import React from 'react'
 //import { createUseStyles } from "react-jss";
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import axios from 'axios';
 import { BASE_URL } from '../utils/base_url';
 
@@ -11,30 +11,58 @@ import { BASE_URL } from '../utils/base_url';
 const Landing = () => {
     //const classes = useStyles();
     const wordRef = useRef(); 
+    const [match, setMatch] = useState()
+    const [wordNum, setWordNumb] = useState(0)
+    const [wordFound, setWordFound] = useState()
 
-    const getWords = async () => {
-      
+    const getWords = async (word) => {
+ 
       try {
           const res = await axios.get(`${BASE_URL}/scrapy`, {
           })
-          console.log(res)
+          
+          const wordObject = res.data.data[0].words
+
+          for (const w in wordObject) {
+            if (w === word) {
+              setMatch(w)
+              setWordNumb(wordObject[w])
+              setWordFound(true)
+              return
+            }
+            setWordFound(false)
+          }
+
       } catch (error) {
           console.error(error.response.data)
       }
   
     }
 
+    const result = wordFound === true ? (
+      <h3>{match}: {wordNum}</h3>
+    ) : wordFound === false ? (
+      <h3>Word not found</h3>
+    ) : (
+      <></>
+    )
+
+    const handleSubmit = (e) => {
+      e.preventDefault()
+      getWords(wordRef.current.value)
+    }
+
   return (
     <>
-        <h2>Word count comparison</h2>
-        <h3>Word to compare: </h3>
+        <h2>Word count</h2>
+        <h3>Word to count: </h3>
         <input
             type='text'
             ref={wordRef}
             required>
         </input>
-        <input onClick={getWords} type="submit" value="Check"></input>
-        <></>
+        <input onClick={handleSubmit} type="submit" value="Check"></input>
+        {result}
     </>
   )
 }
