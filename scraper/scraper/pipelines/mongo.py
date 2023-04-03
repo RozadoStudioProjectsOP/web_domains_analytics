23d9d12e8553da73bd1ae0c4dbfd33c7cbecf67d
LennoxHB <bladlh1@student.op.ac.nz>
2023-04-03 12:05:56 +1200

    feat: add domain to data

--- scraper/scraper/pipelines/mongo.py
+++ scraper/scraper/pipelines/mongo.py
@@ -7,27 +7,26 @@ from ..items import WordsItem
 import json
 
 class MongoDBPipeline:
-    item = WordsItem()
+    item = {}
+    item['domain'] = ''
 
-    # def __init__(self):
-    #     dotenv.load_dotenv()
-    #     MONGO_URI = os.environ["MONGO_URI"]
-    #     MONGO_DB =  os.environ["MONGO_DB"]
-    #     MONGO_COLLECTION =  os.environ["MONGO_COLLECTION"]
-    #     self.client = pymongo.MongoClient(MONGO_URI)
-    #     self.db = self.client[MONGO_DB]
-    #     self.col = self.db[MONGO_COLLECTION]
-    #     self.col.delete_many({})
+    def __init__(self):
+        dotenv.load_dotenv()
+        MONGO_URI = os.environ["MONGO_URI"]
+        MONGO_DB =  os.environ["MONGO_DB"]
+        MONGO_COLLECTION =  os.environ["MONGO_COLLECTION"]
+        self.client = pymongo.MongoClient(MONGO_URI)
+        self.db = self.client[MONGO_DB]
+        self.col = self.db[MONGO_COLLECTION]
 
     def close_spider(self, spider):
-        # self.col.insert_one(dict(self.item))
-        # self.client.close()
-        self.file = open('data.json', 'w')
-        line = json.dumps(dict(self.item)) + "\n"
-        self.file.write(line)
-        self.file.close()
+        query = { 'domain': self.item['domain'] }
+        data = dict(self.item)
+        self.col.update_one(query, { '$set':  data }, upsert=True)
+        self.client.close()
     
     def process_item(self, item, spider):
         self.item = item
+        self.item['domain'] = spider.url
 
         
