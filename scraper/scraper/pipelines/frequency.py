64428041a579324112f3e3721b46a81b8b702732
LennoxHB <bladlh1@student.op.ac.nz>
2023-04-03 12:04:52 +1200

    feat: show frequency and total

--- scraper/scraper/pipelines/frequency.py
+++ scraper/scraper/pipelines/frequency.py
@@ -3,9 +3,10 @@ from collections import Counter
 class FrequencyPipeline:
     item = {}
     item['words'] = {}
+
     
-    def process_item(self, item, spider):
+    def process_item(self, item, spider): 
         count = Counter(item['words']).most_common(100)
         for word in count:
-            self.item['words'][word[0]] = word[1]/item['count']
+            self.item['words'][word[0]] = { 'Frequency': word[1]/item['count'], 'Total': word[1]}
         return self.item
