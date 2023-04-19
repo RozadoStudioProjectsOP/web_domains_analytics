a292d291a8208a0476d7b32f23af5660837af57f
unknown <altaay1@student.op.ac.nz>
2023-04-19 18:00:36 +1200

    feat: setup routes for google oauth

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,18 @@
 import { Router } from "express";
+import passport from "passport";
 const router = Router();
 
 import { register, login, logout } from "../controllers/auth.js"
 
+// Auth Routes
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/logout").post(logout);
+
+// Google OAuth
+router.route('/google').get(passport.authenticate('google', { scope: ['profile', 'email'] }));
+router.route('/google/callback').get(passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
+    res.redirect('http://localhost:3001/');
+});
 
 export default router;
