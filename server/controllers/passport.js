fd3c33e699a4fe45bc649634d66dea0d6879bcdd
unknown <altaay1@student.op.ac.nz>
2023-04-19 18:01:10 +1200

    feat: setup passport strategy for facebook/github

--- server/controllers/passport.js
+++ server/controllers/passport.js
@@ -1,6 +1,8 @@
 import passport from "passport";
 import dotenv from "dotenv";
 import GoogleStrategy from 'passport-google-oauth20';
+import FacebookStrategy from 'passport-facebook';
+import GitHubStrategy from 'passport-github2';
 import User from "../models/user.js";
 
 dotenv.config();
@@ -15,6 +17,65 @@ passport.use(
     }, (accessToken, refreshToken, profile, done) => {
         // passport callback function
         console.log('google passport callback function fired');
+        User.findOne({ accountId: profile.id, provider: profile.provider }).then((currentUser) => {
+            if (currentUser !== null) {
+                console.log(currentUser)
+                done(null, currentUser);
+            } else {
+                const userData = {
+                    accountId: profile.id,
+                    username: profile.displayName,
+                    email: profile.emails[0].value,
+                    provider: profile.provider,
+                }
+                console.log(userData)
+                User.create(userData).then((newUser) => {
+                    done(null, newUser);
+                });
+            }
+        });
+    })
+);
+
+passport.use(
+    new FacebookStrategy({
+        // options for facebook strategy
+        clientID: `${process.env.FACEBOOK_CLIENT_ID}`,
+        clientSecret: `${process.env.FACEBOOK_CLIENT_SECRET}`,
+        callbackURL: `${process.env.CALLBACK_URL}/auth/facebook/callback`,
+    }, (accessToken, refreshToken, profile, done) => {
+        // passport callback function
+        console.log('facebook passport callback function fired');
+        User.findOne({ accountId: profile.id, provider: profile.provider }).then((currentUser) => {
+            if (currentUser !== null) {
+                console.log(currentUser)
+                done(null, currentUser);
+            } else {
+                const userData = {
+                    accountId: profile.id,
+                    username: profile.displayName,
+                    provider: profile.provider,
+                }
+                console.log(userData)
+                User.create(userData).then((newUser) => {
+                    done(null, newUser);
+                });
+            }
+        });
+    })
+);
+
+passport.use(
+    new GitHubStrategy({
+        // options for github strategy
+        clientID: `${process.env.GITHUB_CLIENT_ID}`,
+        clientSecret: `${process.env.GITHUB_CLIENT_SECRET}`,
+        callbackURL: `${process.env.CALLBACK_URL}/auth/github/callback`,
+        scope: ['profile', 'user:email'],
+    }, (accessToken, refreshToken, profile, done) => {
+        // passport callback function
+        console.log('github passport callback function fired');
+        console.log(profile)
         User.findOne({ accountId: profile.id, provider: profile.provider }).then((currentUser) => {
             if (currentUser !== null) {
                 console.log(currentUser)
