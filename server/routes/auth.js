330933478d82609d265bb3bf0ac57d03b8fe75e1
unknown <altaay1@student.op.ac.nz>
2023-04-19 18:01:33 +1200

    feat: add routes for facebook/github oauth

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -15,4 +15,16 @@ router.route('/google/callback').get(passport.authenticate('google', { failureRe
     res.redirect('http://localhost:3001/');
 });
 
+// Facebook OAuth
+router.route('/facebook').get(passport.authenticate('facebook', { scope: ['profile', 'email'] }));
+router.route('/facebook/callback').get(passport.authenticate('facebook', { failureRedirect: '/' }), (req, res) => {
+    res.redirect('http://localhost:3001');
+});
+
+// Github OAuth
+router.route('/github').get(passport.authenticate('github', { scope: ['profile', 'email'] }));
+router.route('/github/callback').get(passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
+    res.redirect('http://localhost:3001');
+});
+
 export default router;
