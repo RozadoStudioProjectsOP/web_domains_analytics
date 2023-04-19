00d64d27ddd0f2e677a5e7df4cf3edc78786b8b5
unknown <altaay1@student.op.ac.nz>
2023-04-19 17:16:55 +1200

    feat: create passport strategy for google oauth

---
+++ server/controllers/passport.js
@@ -0,0 +1,43 @@
+import passport from "passport";
+import GoogleStrategy from 'passport-google-oauth20';
+import User from "../models/user.js";
+
+passport.use(
+    new GoogleStrategy({
+        // options for google strategy
+        callbackURL: `${process.env.CALLBACK_URL}/auth/google/callback`,
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        scope: ['profile', 'email']
+    }, (accessToken, refreshToken, profile, done) => {
+        // passport callback function
+        console.log('google passport callback function fired');
+        User.findOne({ accountId: profile.id, provider: profile.provider }).then((currentUser) => {
+            if (currentUser !== null) {
+                console.log(currentUser)
+                done(null, currentUser);
+            } else {
+                const userData = {
+                    accountId: profile.id,
+                    username: profile.displayName,
+                    email: profile.emails[0].value,
+                    provider: profile.provider,
+                }
+                console.log(userData)
+                User.create(userData).then((newUser) => {
+                    done(null, newUser);
+                });
+            }
+        });
+    })
+);
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+
+export default passport;
