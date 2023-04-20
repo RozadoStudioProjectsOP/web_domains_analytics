b3cecaf0d2658afd873892fe6faa4828d377a0c6
unknown <altaay1@student.op.ac.nz>
2023-04-21 04:42:01 +1200

    refactor: update url for social login func and store login status

--- client/src/components/OAuth/Loginfunc.js
+++ client/src/components/OAuth/Loginfunc.js
@@ -1,28 +1,32 @@
+import React from "react";
+import { BASE_URL } from "../../utils/base_url.js";
+
 const SocialLogins = () => {
     const googleLogin = async () => {
-        window.open("http://localhost:3000/auth/google/callback", "_self");
+        window.open(`${BASE_URL}/auth/google/callback`, "_self");
+        sessionStorage.setItem('isLoggedIn', true);
     }
 
     const facebookLogin = () => {
-        window.open("http://localhost:3000/auth/facebook/callback", "_self");
+        window.open(`${BASE_URL}/auth/facebook/callback`, "_self");
+        sessionStorage.setItem('isLoggedIn', true);
     }
 
     const githubLogin = () => {
-        window.open("http://localhost:3000/auth/github/callback", "_self");
+        window.open(`${BASE_URL}/auth/github/callback`, "_self");
+        sessionStorage.setItem('isLoggedIn', true);
     }
 
     return (
-        <div >
-            <div>
-                <div onClick={googleLogin}>
-                    <p>Login With Google</p>
-                </div>
-                <div onClick={facebookLogin}>
-                    <p>Login With Facebook</p>
-                </div>
-                <div onClick={githubLogin}>
-                    <p>Login With Github</p>
-                </div>
+        <div>
+            <div onClick={googleLogin}>
+                <p>Login With Google</p>
+            </div>
+            <div onClick={facebookLogin}>
+                <p>Login With Facebook</p>
+            </div>
+            <div onClick={githubLogin}>
+                <p>Login With Github</p>
             </div>
         </div>
     )
