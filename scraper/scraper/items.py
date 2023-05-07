721e17422474e9b750e0706d86da41a9995b9355
LennoxHB <bladlh1@student.op.ac.nz>
2023-05-07 19:09:00 +1200

    fix: fix typo

--- scraper/scraper/items.py
+++ scraper/scraper/items.py
@@ -2,7 +2,7 @@ from scrapy.item import Item, Field
 
 class DomainAnalyitcs(Item):
     raw = Field()
-    count = Field()
+    counts = Field()
     domain = Field()
     words = Field()
     bigrams = Field()
