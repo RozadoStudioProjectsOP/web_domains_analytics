68c8845875ee9a10a490b3eaa957ab660520a072
LennoxHB <bladlh1@student.op.ac.nz>
2023-05-07 23:19:05 +1200

    fix: calculate frequency correctly

--- scraper/scraper/pipelines/count.py
+++ scraper/scraper/pipelines/count.py
@@ -8,24 +8,24 @@ class CountPipeline:
         item = DomainAnalyitcs(item)
 
         # Count total words and ngrams
-        item['counts'] = {}
+        
+        item['counts'] = {}   
         item['counts']['words'] = len(item['words'])
         item['counts']['bigrams'] = len(item['bigrams'])
         item['counts']['trigrams'] = len(item['trigrams'])
         
-        def countWords(wordList, total):
+        def countWords(wordList):
             # Count individual words
             wordList =  dict(Counter(wordList))
-            # Format the data to better show total, also calculate relative frequency.
+            # Format the data to show total as dict, and to make easy to add other dicts to words.
             for word, count in wordList.items():
                 wordList[word] = {
-                    'Total': count,
-                    'Frequency': count / total
+                    'Total': count
                 }                
             return wordList
 
-        item['words'] = countWords(item['words'], len(item['words']))
-        item['bigrams'] = countWords(item['bigrams'], len(item['bigrams']))
-        item['trigrams'] = countWords(item['trigrams'], len(item['trigrams']))
+        item['words'] = countWords(item['words'])
+        item['bigrams'] = countWords(item['bigrams'])
+        item['trigrams'] = countWords(item['trigrams'])
 
         return item
--- scraper/scraper/pipelines/mongo.py
+++ scraper/scraper/pipelines/mongo.py
@@ -16,6 +16,11 @@ class MongoDBPipeline:
         'bigrams': {},
         'trigrams': {}
     }
+    counts = {
+        'words': 0,
+        'bigrams': 0,
+        'trigrams': 0
+    }
 
     def __init__(self):
         # Establishes a connection to the DB
@@ -29,6 +34,16 @@ class MongoDBPipeline:
 
     def close_spider(self, spider):
         # Tries to update a document or creates a new one based on the given domain.
+        
+        def calculateFrequency(target):
+            #Calculate relative frequency of words
+            for word in self.payload[target]:
+                self.payload[target][word]['Frequency'] = self.payload[target][word]['Total'] / self.counts[target]
+
+        calculateFrequency('words')
+        calculateFrequency('bigrams')
+        calculateFrequency('trigrams')
+
         query = { 'domain': self.payload['domain'] }
         data = dict(self.payload)        
         self.col.update_one(query, { '$set': data }, upsert=True)
@@ -39,18 +54,21 @@ class MongoDBPipeline:
 
         self.payload['domain'] = item['domain']
 
+        self.counts['words'] += item['counts']['words']
+        self.counts['bigrams'] += item['counts']['bigrams']
+        self.counts['trigrams'] += item['counts']['trigrams']
+
         def buildPayload(wordList, target):
             for key, value in wordList.items():                
                 # Check if word already exists in payload.
                 if key in self.payload[target]:
                     # Adds up totals.
                     self.payload[target][key]['Total'] = self.payload[target][key]['Total'] + wordList[key]['Total']
-                    # Averages frequencies.
-                    self.payload[target][key]['Frequency'] = (self.payload[target][key]['Frequency'] + wordList[key]['Frequency']) / 2
                 else:
                     # Adds new word to payload.
                     self.payload[target][key] = value
 
         buildPayload(item['words'], 'words')
         buildPayload(item['bigrams'], 'bigrams')
-        buildPayload(item['trigrams'], 'trigrams')           
+        buildPayload(item['trigrams'], 'trigrams')       
+       
