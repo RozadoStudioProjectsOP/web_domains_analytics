14e4d57249c28c48469d094236c6e84ac03237d4
LennoxHB <bladlh1@student.op.ac.nz>
2023-05-07 12:51:22 +1200

    refactor: improve sanitizer

--- scraper/scraper/pipelines/sanitiser.py
+++ scraper/scraper/pipelines/sanitiser.py
@@ -1,15 +1,24 @@
 #Prcoess the data from the spider into a dictionary that counts the freqency of words.
 import re
+from nltk.corpus import stopwords
 
 class SanitiserPipeline:
     
-    def process_item(self, item, spider):
-        #The scraper returns a list with any text from each elements on the crawled page, we first join
-        #the text together, then make it all lower case so we don't get duplicates of different cases,
-        #then split any text seperated by space into a list.
-        data = ''.join(item['text']).lower().split()
+           
 
-        #Removes special characters, leaves only a-z.
-        words = [re.sub('[^a-z]', '', word) for word in data]        
-        
+    def process_item(self, item, spider):  
+        stop_words = set(stopwords.words('english'))        
+
+        # Takes the raw data from the spider and creates a list of unigrams, while also setting
+        # all characters to lowercase.
+        data = item['text'].lower().split()
+
+        # Remove single quotes, except where used as apostrophees
+        words = [re.sub('/\s\'|\'\s', '', word) for word in data]       
+        # Removes any character that is not lower case alphabet or single quote.
+        words = [re.sub('[^a-z\']', '', word) for word in words]   
+    
+        #Removes stopwords
+        words[:] = [word for word in words if word not in stop_words and word != ''] 
+
         return words
