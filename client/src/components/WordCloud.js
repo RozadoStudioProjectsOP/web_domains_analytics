7b3e994f1ed4ae2145d6b99d0140b7ba514244fd
Miguel Aladro Estrada (1000103361) <ALADM1@student.op.ac.nz>
2023-05-11 13:13:50 +1200

    (fix) reverse updates in wordcloud

--- client/src/components/WordCloud.js
+++ client/src/components/WordCloud.js
@@ -4,60 +4,52 @@ import { useEffect, useState, useCallback } from 'react';
 
 const Wordcloud = (props) => {
 
-  const [data, setData] = useState([{
-      Total: 0,
-      Frequency: 0,
-      text: ''
-    }]);
-    //console.log(data)
-      // Modify data to have a numeric index 
-    const processData = (datas) => {
-      let allData = Object.values(datas)
-      let keys = Object.keys(datas)
-      if (allData) {
-          //let totalWords = allData.length
-          for (let i = 0; i < allData.length; i++) {
-            // console.log(totalWords)
-            allData[i].text = keys[i] 
-            // allData[i].Total = totalWords / allData.length
-            // totalWords--       
-          }
-        }
-      
-      allData.sort((a, b) => {
-        if (a.Total > b.Total) {
-          return -1;
-        }
-        if (a.Total < b.Total) {
-          return 1;
-        }
-        return 0;
-      })          
-        setData(allData.slice(0, 100))    
-    }
+    const [data, setData] = useState([{
+        Total: 0,
+        Frequency: 0,
+        text: ''
+      }]);
+      //console.log(data)
+      useEffect (() => {
+        // Modify data to have a numeric index
+        if(props.data){
+  
+          let allData = Object.values(props.data)
+  
+          let keys = Object.keys(props.data)
+          if (allData) {
+              //let totalWords = allData.length
+              for (let i = 0; i < allData.length; i++) {
+                // console.log(totalWords)
+                allData[i].text = keys[i] 
+                // allData[i].Total = totalWords / allData.length
+                // totalWords--       
+              }
+            }
+          
+          allData.sort((a, b) => {
+            if (a.Total > b.Total) {
+              return -1;
+            }
+            if (a.Total < b.Total) {
+              return 1;
+            }
+            return 0;
+          })          
+            setData(allData.slice(0, 100))
+        }     
+      },[props]);
 
-    useEffect (() => {
-      if (props.mode === 'words'){
-        processData(props.data) 
-      };
-      if (props.mode === 'bigrams'){
-        processData(props.bigrams) 
-      };
-      if (props.mode === 'trigrams'){
-        processData(props.trigrams) 
-      };
-    },[props]);
+      const calculateFontSize = useCallback((word) => Math.sqrt(word.Frequency) * 900, []);
 
-    const calculateFontSize = useCallback((word) => Math.sqrt(word.Frequency) * 900, []);
-
-    const options = useCallback(() => ({
-      font: 'Verdana',
-      fontStyle: 'italic',
-      fontWeight: 'bold',
-      padding: 2,
-      width: 1400,
-      rotate: 0,
-    }), []);
+      const options = useCallback(() => ({
+        font: 'Verdana',
+        fontStyle: 'italic',
+        fontWeight: 'bold',
+        padding: 2,
+        width: 1400,
+        rotate: 0,
+      }), []);
 
   return (
     <WordCloud
