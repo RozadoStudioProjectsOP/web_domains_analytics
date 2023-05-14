15dd8c9f9791d4fb88088172f6786bf737a6200c
Miguel <aladm1@student.op.ac.nz>
2023-05-15 08:38:31 +1200

    fix lint

--- client/src/components/Histogram.js
+++ client/src/components/Histogram.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React from 'react'
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import { useState, useEffect } from 'react';
