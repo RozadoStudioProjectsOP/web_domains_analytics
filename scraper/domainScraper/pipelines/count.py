709d92cfe5830d6032eae3920baa0810cc2e21ff
Lennox Blades (1000013880) <BLADLH1@student.op.ac.nz>
2023-07-17 13:34:26 +1200

    docs: add comments to count pipeline

--- scraper/domainScraper/pipelines/count.py
+++ scraper/domainScraper/pipelines/count.py
@@ -1,21 +1,21 @@
-import re
+# Pipeline used to count the absolute frequency of words.
+
 from ..items import DomainAnalyitcs
 from collections import Counter
 
 class CountPipeline:
     
-    def process_item(self, item, spider):        
+    def process_item(self, item, spider):
         item = DomainAnalyitcs(item)
 
-        # Count total words and ngrams
-        
+        # Count the total words in each category.        
         item['counts'] = {}   
         item['counts']['words'] = len(item['words'])
         item['counts']['bigrams'] = len(item['bigrams'])
         item['counts']['trigrams'] = len(item['trigrams'])
         
         def countWords(wordList):
-            # Count individual words
+            # Count the occurance of each word, or word phrase (bigrams, etc.).
             wordList =  dict(Counter(wordList))
             # Format the data to show total as dict, and to make easy to add other dicts to words.
             for word, count in wordList.items():
