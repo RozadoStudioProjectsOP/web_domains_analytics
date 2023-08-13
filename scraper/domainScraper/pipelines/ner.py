1c5302c246430b8c66e68c6d91f6ab749d341750
LennoxHB <bladlh1@student.op.ac.nz>
2023-08-13 14:51:17 +1200

    feat: Create a list of named entities from scraped data

---
+++ scraper/domainScraper/pipelines/ner.py
@@ -0,0 +1,23 @@
+# Pipeline used to count the absolute frequency of words.
+
+from ..items import DomainAnalyitcs
+from transformers import pipeline, AutoTokenizer, AutoModelForTokenClassification
+
+class NamedEntityRecognitionPipeline:
+    
+    def process_item(self, item, spider):
+        item = DomainAnalyitcs(item)
+
+        tokenizer = AutoTokenizer.from_pretrained("dslim/bert-base-NER")
+        model = AutoModelForTokenClassification.from_pretrained("dslim/bert-base-NER")
+        nlp = pipeline("ner", model=model, tokenizer=tokenizer)
+
+        ner_results = nlp(item['raw'])
+        item['ner'] = []
+        for result in ner_results:
+            if result['entity'][0] == 'B':
+                item['ner'].append(result['word'])
+            elif result['entity'][0] == 'I':
+                item['ner'][len(item['ner']) - 1] = item['ner'][len(item['ner']) - 1] + " " + result['word']
+                
+        return item
