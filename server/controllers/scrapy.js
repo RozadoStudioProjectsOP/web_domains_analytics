04644653f188f3711213844af3e5c7177192bca0
unknown <altaay1@student.op.ac.nz>
2023-08-13 16:38:47 +1200

    docs: add comments to scrape function

--- server/controllers/scrapy.js
+++ server/controllers/scrapy.js
@@ -1,33 +1,40 @@
 // controller for handling scraped data
-import axios from 'axios';
+import axios from "axios";
 import Data from "../models/data.js";
 
 import { MONGO_URI, DB, PROJECT, SCRAPYD_URL } from "../utils/envSetup.js";
 
+/**
+ * Function to scrape a website
+ * Sends a POST request to the Scrapyd API to schedule a scraping job
+ * @param {*} req
+ * @param {*} res
+ */
 const scrape = async (req, res) => {
-    const { url } = req.body;
-    try {
+  const { url } = req.body;
+  try {
     const response = await axios.post(`${SCRAPYD_URL}/schedule.json`,
+      // arguments for scheduling a scraping job
       new URLSearchParams({
-          'project': PROJECT,
-          'spider': 'spider',
-          'url': url,
-          'MONGO_DB': DB,
-          'MONGO_URI': MONGO_URI,
-          'MONGO_COLLECTION': Data.collection.name,
-      }),
-    )
+        project: PROJECT,
+        spider: "spider",
+        url: url,
+        MONGO_DB: DB,
+        MONGO_URI: MONGO_URI,
+        MONGO_COLLECTION: Data.collection.name,
+      })
+    );
     return res.status(200).json({ success: true, data: response.data });
-    } catch (err) {
-        return res.status(500).json({
-            msg: err.message || "Something went wrong while getting data.",
-        });
-    }
-}
+  } catch (err) {
+    return res.status(500).json({
+      msg: err.message || "Something went wrong while getting data.",
+    });
+  }
+};
 
 const getDomains = async (req, res) => {
   try {
-    const data = await Data.find().distinct('domain');
+    const data = await Data.find().distinct("domain");
     return res.status(200).json({ success: true, data: data });
   } catch (err) {
     return res.status(500).json({
